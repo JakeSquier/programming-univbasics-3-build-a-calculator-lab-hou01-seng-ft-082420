34306f421fe4e97080dfa7f22cb547e3b55145ec
Jacob Squier <jacob.squier@gmail.com>
2020-06-20 07:19:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,27 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  diff = num1 - num2
+  puts diff
+  return diff
 end
 
 def division(num1, num2)
-
+  divis = num1/num2
 end
 
 def multiplication(num1, num2)
-
+  multi = num1 * num2
+  puts multi
+  return multi
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  puts mod
+  return mod
 end
 
 def square_root(num)
-
+  sqr = 
 end
